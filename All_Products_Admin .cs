0b3e37d91da2f24dad5fb520b4f16e1d5435bfc2
MD-YUSUF-22 <mdyusuf47461@gmail.com>
2025-01-20 23:19:10 +0600

    Update 1.2

--- All_Products_Admin .cs
+++ All_Products_Admin .cs
@@ -129,12 +129,7 @@ namespace ProjectWin
             if (updateBtn.Text == "Edit")
             {
                 updateBtn.Text = "Update";
-                gameID.ReadOnly = false;
-                gameDiscount.ReadOnly = false;
-                gameGenre.ReadOnly = false;
-                gameName.ReadOnly = false;
-                gameStock.ReadOnly = false;
-                gamePrice.ReadOnly = false;
+               
 
 
             }
