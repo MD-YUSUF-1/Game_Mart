8fa90da9b6517b81aeca35b83713b968612ee5e9
Ankon 190 <ankon190@gmail.com>
2025-01-20 23:00:27 +0600

    modified design

---
+++ Admin_SalesPerson_Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectWin
+{
+    public partial class Admin_SalesPerson_Table : Form
+    {
+        public Admin_SalesPerson_Table()
+        {
+            InitializeComponent();
+        }
+    }
+}
