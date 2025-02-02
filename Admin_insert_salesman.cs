d5914ab9f33a932868bcc247238a41244df72c3e
Ankon 190 <ankon190@gmail.com>
2025-02-02 11:34:10 +0600

    New 2 page added

---
+++ Admin_insert_salesman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectWin
+{
+    public partial class Admin_insert_salesman : Form
+    {
+        public Admin_insert_salesman()
+        {
+            InitializeComponent();
+        }
+    }
+}
