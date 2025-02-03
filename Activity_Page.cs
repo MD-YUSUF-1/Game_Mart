69c6bfd1f7907ff6fc3b744473179dfe78131d7a
Ankon 190 <ankon190@gmail.com>
2025-02-04 00:04:32 +0600

    Activity page code added

---
+++ Activity_Page.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace ProjectWin
+{
+    public partial class Activity_Page : Form
+    {
+        public Activity_Page()
+        {
+            InitializeComponent();
+            Form2_Load();
+        }
+
+        SqlConnection con;
+        //Databse connection code 
+        public void dbcon()
+        {
+            try
+            {
+                con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\C# Project\database\database\Game_Mart.mdf"";Integrated Security=True;Connect Timeout=30;Encrypt=False
+");
+                con.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to connect to the database: {ex.Message}", "Database Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form2_Load()
+        {
+            try
+            {
+                dbcon();
+                SqlCommand sq1 = new SqlCommand("select * from Activity_Table", con);
+                SqlDataAdapter sda = new SqlDataAdapter(sq1);
+                DataTable dt = new DataTable();
+
+                sda.Fill(dt);
+                dataGridView1.RowTemplate.Height = 75;
+                dataGridView1.DataSource = dt;
+                DataGridViewImageColumn img = new DataGridViewImageColumn();
+               // img = (DataGridViewImageColumn)dataGridView1.Columns[6];
+               // img.ImageLayout = DataGridViewImageCellLayout.Stretch;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load Table " + ex);
+            }
+        }
+
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
