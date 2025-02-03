2db9f2464722c5f669e572812fbd1cf85fb24820
Ankon 190 <ankon190@gmail.com>
2025-02-03 12:52:15 +0600

    1 page added

---
+++ Manager_all_product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectWin
+{
+    public partial class Manager_all_product : Form
+    {
+        public Manager_all_product()
+        {
+            InitializeComponent();
+        }
+    }
+}
